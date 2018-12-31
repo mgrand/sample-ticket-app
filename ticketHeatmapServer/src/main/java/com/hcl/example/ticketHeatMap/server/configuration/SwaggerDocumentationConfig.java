d9aa19c701a97a4f6dfbbd062d32be52b0a18da8
Mark Grand <40677227+mgrand-papajohns@users.noreply.github.com>
2018-12-31 14:55:56 -0500

    Reorganized package structure.

--- ticketHeatmapServer/src/main/java/com/hcl/example/ticketHeatMap/server/configuration/SwaggerDocumentationConfig.java
+++ ticketHeatmapServer/src/main/java/com/hcl/example/ticketHeatMap/server/configuration/SwaggerDocumentationConfig.java
@@ -10,11 +10,10 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-12-30T19:37:21.031Z")
-
 @Configuration
 public class SwaggerDocumentationConfig {
 
+    @SuppressWarnings("WeakerAccess")
     ApiInfo apiInfo() {
         return new ApiInfoBuilder()
             .title("Issues by region API")
@@ -31,7 +30,7 @@ public class SwaggerDocumentationConfig {
     public Docket customImplementation(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                    .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                    .apis(RequestHandlerSelectors.basePackage("com.hcl.example.ticketHeatMap"))
                     .build()
                 .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
