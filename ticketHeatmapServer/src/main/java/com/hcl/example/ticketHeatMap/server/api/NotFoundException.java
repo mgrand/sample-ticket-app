791b2910976855505059c70c1ed355e7a734cbeb
Mark Grand <40677227+mgrand-papajohns@users.noreply.github.com>
2019-01-02 15:57:23 -0500

    Resolved a warning.

--- ticketHeatmapServer/src/main/java/com/hcl/example/ticketHeatMap/server/api/NotFoundException.java
+++ ticketHeatmapServer/src/main/java/com/hcl/example/ticketHeatMap/server/api/NotFoundException.java
@@ -1,9 +1,7 @@
 package com.hcl.example.ticketHeatMap.server.api;
 
 public class NotFoundException extends ApiException {
-    private int code;
     public NotFoundException (int code, String msg) {
         super(code, msg);
-        this.code = code;
     }
 }
